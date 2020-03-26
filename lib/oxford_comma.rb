0012f60ef326b8d794b2d4e63cde464aea8ee749
Heather Moreira <moreiraheather@gmail.com>
2020-03-26 21:23:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,7 +12,7 @@ def oxford_comma(array)
   if array.length == 2
     return array.join(" and ")
   elsif array.length > 2
-    last_element = array.pop  #take of last element 
+    last_element = array.pop  
     array.join(", ") << ", and #{last_element}" 
   else 
     return array.join() 
